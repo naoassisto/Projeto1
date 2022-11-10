563c6bae836af104f7b220f5555d9ed047e5f198
Alexandre Fonseca <80794067+Xandebrabe@users.noreply.github.com>
2022-11-10 13:56:18 -0300

    Main Rastreio Att

--- src/Frontend/src/index.js
+++ src/Frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import MainRastreio from './pages/PrincipalRastreio';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/Frontend/src/pages/PrincipalRastreio.js
+++ src/Frontend/src/pages/PrincipalRastreio.js
@@ -1,23 +1,24 @@
 import { Box, TextField, Typography } from "@mui/material";
 import NavigationBar from "../services/navbar";
+import Button from "@mui/material/Button";
 
 function MainRastreio() {
     return (
         <>
             <NavigationBar></NavigationBar>
             <div>
-                <Typography>Pesquisar por ID</Typography>
-                <TextField></TextField>
+                <Typography sx={{mt:'36px', ml:'33px', display:'inline'}}>Pesquisar por ID</Typography>
+                <TextField sx={{ml:'2px', display:'inline'}}></TextField>
             </div>
             <div>
-                <Typography>Pesquisa por Tipo</Typography>
-                <TextField></TextField>
+                <Typography sx={{display:'inline', ml:'15px'}}>Pesquisa por Tipo</Typography>
+                <TextField sx={{display:'inline', ml:'2px'}}></TextField>
             </div>
-            <Box>
-                <Button></Button>
-                <Button></Button>
-                <Button></Button>
-                <Button></Button>
+            <Box sx={{width:'472px', height:'165px', mt:'-110px', ml:'808px', backgroundColor:'#D9D9D9', borderRadius:'24px'}}>
+                <Button sx={{width:'172px', height:'57px', padding: '8px 16px 8px 16px'}}>Remover</Button>
+                <Button>Remover Tudo</Button>
+                <Button>Adicionar</Button>
+                <Button>Revelar Tudo</Button>
             </Box>
         </>
     );
