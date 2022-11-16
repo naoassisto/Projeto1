e63b736b15b011b3485ac70839cf36186379f603
Alexandre Fonseca <80794067+Xandebrabe@users.noreply.github.com>
2022-11-16 13:24:41 -0300

    Get method Added | Have to configure with Aws Meth

--- src/Backend/liondevs/app_30.js
+++ src/Backend/liondevs/app_30.js
@@ -7,6 +7,24 @@ var router = express.Router();
 var http = require('http');
 var path = require("path");
 
+
+// app.get("/readAWSData", (req, res) => { // Método Get, pega as informações disponíveis no EndPoint, sendo assim, é possível pegar os dados enviados da Aws para esse EndPoint
+
+//     res.statusCode = 200;
+
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+
+//     var selectCommand = 'Query SQL';
+
+//     aws.all(sql, [], (err, rows) => {
+//         if (err) {
+//             throw err;
+//         }
+//         res.json(awsData);
+//     });
+//     aws.close();
+// });
+
 // router.get(“/”, function(req, res, next) {
 //     res.send(“API is working properly”);
 // });
