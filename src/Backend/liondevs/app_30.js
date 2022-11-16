18b56afdc68188bf36562870af79f1360e51ae05
Alexandre Fonseca <80794067+Xandebrabe@users.noreply.github.com>
2022-11-16 13:15:45 -0300

    Adding stages API | Adding structure to get data

--- src/Backend/liondevs/app_30.js
+++ src/Backend/liondevs/app_30.js
@@ -1,6 +1,14 @@
 const hostname = '127.0.0.1'; // IP Adress
 const port = 3030; // Door
 
+var express = require(“express”);
+var router = express.Router();
+
 var http = require('http');
 var path = require("path");
 
+// router.get(“/”, function(req, res, next) {
+//     res.send(“API is working properly”);
+// });
+
+// module.exports = router;
--- src/Frontend/src/pages/Home.js
+++ src/Frontend/src/pages/Home.js
@@ -3,28 +3,45 @@ import Button from '@mui/material/Button';
 import { Typography } from "@mui/material";
 
 function Home() {
-    return (
-        <>
-        <NavigationBar></NavigationBar>
-        <div>
-                <Button sx={{bgcolor:"#0082AF", borderRadius:'90px', width:'337px', height:'69px',display:'inline-block', mt:'549px', ml:'95px' }}>
-                    <Typography sx={{color:'#000000'}}>
-                        Adicionar Dispositivo
-                    </Typography>
-                </Button>
-                <Button sx={{bgcolor:"#0082AF", borderRadius:'90px', width:'337px', height:'69px', display:'inline-block', mt:'549px', ml:'95px'}}>
-                    <Typography sx={{color:'#000000'}}>
-                        Rastrear Dispositivos
-                    </Typography>
-                </Button>
-                <Button sx={{bgcolor:"#0082AF", borderRadius:'90px', width:'337px', height:'69px', display:'inline-block', mt:'549px', ml:'95px'}}>
-                    <Typography sx={{color:'#000000'}}>
-                        Gerar Relatórios
-                    </Typography>
-                </Button>
-        </div>
-        </>
-    );
-  }
+        // constructor(props) {
+        //     super(props);
+        //     this.state = { apiResponse: "" };
+        // }
+
+        // callAPI() {
+        //     fetch("http://localhost:9000/testAPI")
+        //     .then(res => res.text())
+        //     .then(res => this.setState({ apiResponse: res }));
+        // }
+
+        // componentWillMount() {
+        //     this.callAPI();
+        // }
+    // render(){
+        return (
+            <>
+            <NavigationBar></NavigationBar>
+            <div>
+                    <Button sx={{bgcolor:"#0082AF", borderRadius:'90px', width:'337px', height:'69px',display:'inline-block', mt:'549px', ml:'95px' }}>
+                        <Typography sx={{color:'#000000'}}>
+                            Adicionar Dispositivo
+                        </Typography>
+                    </Button>
+                    <Button sx={{bgcolor:"#0082AF", borderRadius:'90px', width:'337px', height:'69px', display:'inline-block', mt:'549px', ml:'95px'}}>
+                        <Typography sx={{color:'#000000'}}>
+                            Rastrear Dispositivos
+                        </Typography>
+                    </Button>
+                    <Button sx={{bgcolor:"#0082AF", borderRadius:'90px', width:'337px', height:'69px', display:'inline-block', mt:'549px', ml:'95px'}}>
+                        <Typography sx={{color:'#000000'}}>
+                            Gerar Relatórios
+                        </Typography>
+                    </Button>
+            </div>
+            </>
+        );
+    }
+    
+//   }
   
   export default Home;
