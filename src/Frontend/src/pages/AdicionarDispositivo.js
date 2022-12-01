6c1c84236e8e07f4d130e5a74a582af06f841be9
Vfernandes1 <vinicius.fernandes@sou.inteli.edu.br>
2022-12-01 10:02:51 -0300

    Algumas estilizações

--- src/Frontend/src/pages/AdicionarDispositivo.js
+++ src/Frontend/src/pages/AdicionarDispositivo.js
@@ -7,24 +7,24 @@ function AdicionarDispositivo() {
         <>
         <NavigationBar></NavigationBar>
 
-            <Typography sx={{height:'28px', width:'319px', mt:'150px', ml:'121px', fontFamily: 'Inter', fontSize: '30px', fontWeight: '400', lineHeight: '36px', letterSpacing: '0em', align: 'left'}}>
+            <Typography sx={{height:'1.75rem', width:'19.94rem', mt:'9.375rem', ml:'7.563rem', fontFamily: 'Inter', fontSize: '1.875rem', fontWeight: '400', lineHeight: '2.25rem', letterSpacing: '0em', align: 'left'}}>
                 Tipo do dispositivo
             </Typography>
             <TextField variant="filled" color="success" sx={{border:'2px solid #000000',backgroundColor:'#ffff', borderRadius:'7px', width:'514px', height:'42px', ml:'467px'}} />
 
-            <Typography sx={{height:'28px', width:'319px', mt:'59px', ml:'121px', fontFamily: 'Inter', fontSize: '30px', fontWeight: '400', lineHeight: '36px', letterSpacing: '0em', align: 'left'}}>
+            <Typography sx={{height:'1.75rem', width:'19.94rem', mt:'3.688rem', ml:'7.563rem', fontFamily: 'Inter', fontSize: '1.875rem', fontWeight: '400', lineHeight: '2.25rem', letterSpacing: '0em', align: 'left'}}>
                 ID do dispositivo
             </Typography>
             <TextField variant="filled" color="success" sx={{border:'2px solid #000000',backgroundColor:'#ffff', borderRadius:'7px', width:'514px', height:'42px', ml:'467px'}} />
 
-            <Typography sx={{height:'28px', width:'319px', mt:'50px', ml:'121px', fontFamily: 'Inter', fontSize: '30px', fontWeight: '400', lineHeight: '36px', letterSpacing: '0em', align: 'left'}}>
+            <Typography sx={{height:'1.75rem', width:'19.94rem', mt:'3.125rem', ml:'7.563rem', fontFamily: 'Inter', fontSize: '1.875rem', fontWeight: '400', lineHeight: '2.25rem', letterSpacing: '0em', align: 'left'}}>
                 Tag do dispositivo
             </Typography>
             <TextField variant="filled" color="success" sx={{border:'2px solid #000000',backgroundColor:'#ffff', borderRadius:'7px', width:'514px', height:'42px', ml:'467px'}} />
 
-            <Button sx={{width:'188px', height:'56.93px', mt:'186px', ml:'257px', backgroundColor:'#34C471', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Adicionar</Button>
-            <Button sx={{width:'213px', height:'56.93px', mt:'186px', ml:'88px', backgroundColor:'#0082AF', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Alterar Tag</Button>
-            <Button sx={{width:'213px', height:'56.93px', mt:'186px', ml:'88px', backgroundColor:'#E64747', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Remover</Button>
+            <Button sx={{width:'188px', height:'56.93px', mt:'11.625rem', ml:'16.063rem', backgroundColor:'#34C471', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Adicionar</Button>
+            <Button sx={{width:'213px', height:'56.93px', mt:'11.625rem', ml:'5.5rem', backgroundColor:'#0082AF', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Alterar Tag</Button>
+            <Button sx={{width:'213px', height:'56.93px', mt:'11.625rem', ml:'5.5rem', backgroundColor:'#E64747', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Remover</Button>
         </>
     );
   }
--- src/Frontend/src/pages/Localizacao.js
+++ src/Frontend/src/pages/Localizacao.js
@@ -1,3 +1,4 @@
+import { Typography } from '@mui/material';
 import React from 'react';
 import logo from '../images/mapa.png'; 
 import NavigationBar from '../services/navbar';
@@ -12,8 +13,8 @@ function Localizacao() {
     <br></br>
     <br></br>
     <div>
-      <h3>Pesquisa por ID</h3>
-      <h3>Pesquisa por ID</h3>
+      <Typography sx={{ml:'1.875rem', fontSize:'24px' }}>Pesquisa por ID</Typography>
+      <Typography sx={{ml:'1.875rem', fontSize:'24px'}}>Pesquisa por ID</Typography>
       <img src={logo} alt="Logo" width="1200" height="700"/>
     </div>
     
